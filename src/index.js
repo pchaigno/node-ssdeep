be2eb517c30d82230f974775552f9ce91c175b53
driscolld <david.driscoll@cfpb.gov>
2017-06-08 22:26:48 -0400

    Fixed interface definition for calling ssdeep library which is a library written in c/c++ an available as an add-in to the operating system(linux) Specifically updated ffi usage.

--- src/index.js
+++ src/index.js
@@ -5,24 +5,31 @@ const SPAMSUM_LENGTH = 64;
 const FUZZY_MAX_RESULT = (2 * SPAMSUM_LENGTH + 20);
 
 var ssdeep = ffi.Library('libfuzzy', {
-  'fuzzy_hash_buf': ['int', ['string', 'uint32', 'string']],
-  'fuzzy_hash_filename': ['int', ['string', 'string']],
+  'fuzzy_hash_buf': ['int', ['string', 'uint32', 'pointer']],
+  'fuzzy_hash_filename': ['int', ['string', 'pointer']],
   'fuzzy_compare': ['int', ['string', 'string']]
 });
 
 module.exports = {
   hash: function(plaintext) {
-    var hashPtr = ref.allocCString(Array(FUZZY_MAX_RESULT).join(' '));
-    res = ssdeep.fuzzy_hash_buf(plaintext, plaintext.length, hashPtr);
+
+    var hashPtr = ref.allocCString(Array(FUZZY_MAX_RESULT).join(' '),'utf8');
+
+    var res = ssdeep.fuzzy_hash_buf(plaintext, plaintext.length, hashPtr);
     if(res != 0) {
       return null;
     }
+
     var hash = hashPtr.readCString();
+
+
     return hash;
   },
 
   hash_from_file: function(filepath) {
+
     var hashPtr = ref.allocCString(Array(FUZZY_MAX_RESULT).join(' '));
+
     var res = ssdeep.fuzzy_hash_filename(filepath, hashPtr);
     if(res != 0) {
       return null;
--- test/test.js
+++ test/test.js
@@ -1,20 +1,67 @@
 var ssdeep = require('../src/index.js');
-var expect = require('chai').expect
+var chai = require('chai');
+var expect = chai.expect;
 var fs = require('fs');
+var should = chai.should();
+var assert = require('assert');
+
+
+
+
 
 describe('ssdeep', function() {
+
+  //mocha sanity check
+  describe('#equal', function () {
+
+    var three = '3';
+    it('should return true that 3 equals "3"', function (done) {
+      assert.equal(three, '3', '3 equals "3"');
+      done();
+    });
+    it('"3" only strictly equals 3.toString()', function (done) {
+      assert.strictEqual(three, '3', '3 equals "3"');
+      done();
+    });
+  });
+
+
   var hash_lorem1 = '192:NAE6bcwSSZcKiBwrOdGrecEFBJSE+51vasvxc2CaB1pqU80oEhwNeLivjlSbLbZv:aEXciJarmD0701o7pACWiV1ut87tD';
 
-  context('compute hash for string', function() {
-    var content; 
+
+  context('test for string', function() {
+    var content;
     beforeEach(function() {
       content = fs.readFileSync('test/lorem1.txt', 'utf8');
     });
-    it('returns the expected hash', function() {
-      var hash = ssdeep.hash(content);
-      expect(hash).to.eql(hash_lorem1);
+    it('returns the expected string', function() {
+
+      var textTxt='hello';
+      expect(textTxt).to.eql('hello');
+
+
     });
   });
+
+  context('compute hash for string', function() {
+    var content;
+    beforeEach(function() {
+      //fs.readFileSync returns buffer if no options specified
+      //if option like utf8 specified returns string
+      content = fs.readFileSync('test/lorem1.txt', 'utf8');
+
+
+    });
+
+    it('returns the expected hash', function() {
+     var ssdeep = require('../src/index.js');
+      var hash = ssdeep.hash(content,'utf8');
+      expect(hash).to.eql(hash_lorem1);
+
+    });
+  });
+
+
 
   context('compute hash for file', function() {
     it('returns the expected hash', function() {
@@ -22,7 +69,7 @@ describe('ssdeep', function() {
       expect(hash).to.eql(hash_lorem1);
     });
   });
-
+  
   context('compute hash for inexistant file', function() {
     it('returns the expected hash', function() {
       var hash = ssdeep.hash_from_file('lorem1.txt');
